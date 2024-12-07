a8f54c3b2a48d51fca11f74cc4756fdea09b43bb
Akhil roczz <b.akhil667@gmail.com>
2024-12-07 23:36:17 +0530

    converts an object into an array of keys and values

---
+++ convertKeysValues.js
@@ -0,0 +1,16 @@
+const objectToArray = (obj) => Object.entries(obj);
+console.log(
+  objectToArray({
+    D: 1,
+    B: 2,
+    C: 3,
+  })
+);
+
+console.log(
+  objectToArray({
+    likes: 2,
+    dislikes: 3,
+    followers: 10,
+  })
+);
