583dabcfea3e7ce2058640a5fab83637ad6720e8
Akhil roczz <b.akhil667@gmail.com>
2024-12-10 23:00:45 +0530

    Special Array

---
+++ specialArray.js
@@ -0,0 +1,13 @@
+function isSpecialArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 == 0 && arr[i] % 2 !== 0) {
+      return false;
+    } else if (i % 2 !== 0 && arr[i] % 2 == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3]));
+console.log(isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3]));
+console.log(isSpecialArray([2, 7, 8, 8, 6, 1, 6, 3]));
