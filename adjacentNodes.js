21597bc159e4f0a7fe48bd4e7d06e580eb1e9bb0
Akhil roczz <b.akhil667@gmail.com>
2024-12-13 21:08:15 +0530

    Adjacent Nodes

---
+++ adjacentNodes.js
@@ -0,0 +1,59 @@
+function areAdjacent(matrix, node1, node2) {
+  if (
+    node1 < 0 ||
+    node2 < 0 ||
+    node1 >= matrix.length ||
+    node2 >= matrix.length
+  ) {
+    return false;
+  }
+  return matrix[node1][node2] === 1 || matrix[node2][node1] === 1;
+}
+console.log(
+  areAdjacent(
+    [
+      [0, 1, 0, 0],
+      [1, 0, 1, 1],
+      [0, 1, 0, 1],
+      [0, 1, 1, 0],
+    ],
+    0,
+    1
+  )
+);
+console.log(
+  areAdjacent(
+    [
+      [0, 1, 0, 0],
+      [1, 0, 1, 1],
+      [0, 1, 0, 1],
+      [0, 1, 1, 0],
+    ],
+    0,
+    2
+  )
+);
+console.log(
+  areAdjacent(
+    [
+      [0, 1, 0, 0],
+      [1, 0, 1, 1],
+      [0, 1, 0, 1],
+      [0, 1, 1, 0],
+    ],
+    0,
+    3
+  )
+);
+console.log(
+  areAdjacent(
+    [
+      [0, 1, 0, 0],
+      [1, 0, 1, 1],
+      [0, 1, 0, 1],
+      [0, 1, 1, 0],
+    ],
+    1,
+    4
+  )
+);
